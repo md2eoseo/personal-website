c3c32098c2ac1e440799c3e1381825c75cfcddca
md2eoseo <md2eoseo@gmail.com>
2020-04-05 22:28:45 +0200

    add class when its on specific block

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", start);
 
+const HTML = {};
+
 function start() {
   console.log("start()");
+
+  HTML.center = document.querySelector("#center");
+  HTML.block = document.querySelectorAll(".block");
+  HTML.block1 = document.querySelector("#block1");
+  HTML.block2 = document.querySelector("#block2");
+  HTML.block3 = document.querySelector("#block3");
+  HTML.block4 = document.querySelector("#block4");
+
+  HTML.block1.addEventListener("mouseover", function () {
+    HTML.block.forEach((ele) => {
+      ele.classList.add("onBlock1");
+    });
+  });
+  HTML.block1.addEventListener("mouseout", function () {
+    HTML.block.forEach((ele) => {
+      ele.classList.remove("onBlock1");
+    });
+  });
 }
